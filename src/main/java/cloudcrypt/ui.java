20215af58a8f3d97e8a343a08a0a90f39da2ee93
Jason Honea <jhonea129@gmail.com>
2019-04-30 00:25:50 -0500

    Working on UI

--- src/main/java/cloudcrypt/ui.java
+++ src/main/java/cloudcrypt/ui.java
@@ -5,42 +5,62 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class ui implements ActionListener{
-    public ui(){
+public class ui implements ActionListener {
+    JTextField encryptPathField;
+    JButton encryptBrowse;
+    JPasswordField encryptKeyField;
+    JPasswordField encryptVerifyField;
+
+    public ui() {
         mainWindow();
     }
 
-    private void mainWindow(){
+    private void mainWindow() {
         JFrame window = new JFrame("Cloud Crypt");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(350,200);
+        window.pack();
         JTabbedPane tabbedPane = new JTabbedPane();
         JPanel encryptPanel = new JPanel();
-        setupEncryptPanel(encryptPanel);
+        setupEncryptPanelTop(encryptPanel);
+        setupEncryptPanelMid(encryptPanel);
         JPanel decryptPanel = new JPanel();
         setupDecryptPanel(decryptPanel);
         tabbedPane.addTab("Encrypt", encryptPanel);
         tabbedPane.addTab("Decrypt", decryptPanel);
         window.add(tabbedPane);
+        window.pack();
         window.setVisible(true);
     }
 
-    private void setupEncryptPanel(JPanel panel){
+    private void setupEncryptPanelTop(JPanel panel) {
+        JPanel textFieldPane = new JPanel();
         JLabel pathLabel = new JLabel("Path to File:");
-        panel.add(pathLabel);
-        JTextField pathField = new JTextField(25);
-        pathField.addActionListener(this);
-        System.out.println(pathField.getText());
-        panel.add(pathField);
+        textFieldPane.add(pathLabel);
+        encryptPathField = new JTextField(25);
+        encryptPathField.addActionListener(this);
+        textFieldPane.add(encryptPathField);
+        encryptBrowse = new JButton("Browse");
+        textFieldPane.add(encryptBrowse);
+        panel.add(textFieldPane);
     }
 
-    private void setupDecryptPanel(JPanel panel){
+    private void setupEncryptPanelMid(JPanel panel) {
+        JPanel textFieldPane = new JPanel();
+        JLabel keyLabel = new JLabel("Key:");
+        textFieldPane.add(keyLabel);
+        encryptKeyField = new JPasswordField(25);
+        textFieldPane.add(encryptKeyField);
+
+        panel.add(textFieldPane);
+    }
+
+    private void setupDecryptPanel(JPanel panel) {
 
     }
 
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == encryptPathField) {
+            System.out.println("Text Updated!");
+        }
     }
 }
-
-
