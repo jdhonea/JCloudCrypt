21e53f95a81faf58b273a440492b9c2ec3c2deac
Jason Honea <jhonea129@gmail.com>
2019-08-06 10:04:49 -0500

    Working on CLI

--- src/main/java/jcloudcrypt/Arguments.java
+++ src/main/java/jcloudcrypt/Arguments.java
@@ -7,8 +7,11 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
 
 public class Arguments {
+    private char selection;
+    private String filePath;
     private Options options;
     private CommandLine arguments;
     private boolean parserError;
@@ -32,6 +35,81 @@ public class Arguments {
         return parserError;
     }
 
+    public char getSelection() {
+        return selection;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public boolean checkForConflicts() {
+        boolean hasEncrypt = arguments.hasOption("encrypt");
+        boolean hasDecrypt = arguments.hasOption("decrypt");
+        // Has both encrypt and decrypt flag
+        if (hasEncrypt && hasDecrypt)
+            return true;
+        // Has neither encrypt or decrypt flag
+        else if (!(hasEncrypt || hasDecrypt))
+            return true;
+        else if (hasEncrypt) {
+            selection = 'e';
+            filePath = arguments.getOptionValue("encrypt");
+        } else if (hasDecrypt) {
+            selection = 'd';
+            filePath = arguments.getOptionValue("decrypt");
+        }
+        return false;
+    }
+
+    public boolean checkOutOfMemBounds() {
+        if (arguments.hasOption("memCost")) {
+            String memCost = arguments.getOptionValue("memCost");
+            if (StringUtils.isNumeric(memCost)) {
+                int memCostVal = Integer.parseInt(arguments.getOptionValue("memCost"));
+                if (memCostVal < 1 || memCostVal > 1024)
+                    return true;
+            } else
+                return true;
+        }
+        return false;
+    }
+
+    public boolean checkOutOfParallelismBounds() {
+        if (arguments.hasOption("parallelism")) {
+            String parallelism = arguments.getOptionValue("parallelism");
+            if (StringUtils.isNumeric(parallelism)) {
+                int parallelismVal = Integer.parseInt(arguments.getOptionValue("parallelism"));
+                if (parallelismVal < 1 || parallelismVal > 32)
+                    return true;
+            } else
+                return true;
+        }
+        return false;
+    }
+
+    public boolean checkOutOfTimeCostBounds() {
+        if (arguments.hasOption("timeCost")) {
+            String timeCost = arguments.getOptionValue("timeCost");
+            if (StringUtils.isNumeric(timeCost)) {
+                int timeCostVal = Integer.parseInt(arguments.getOptionValue("timeCost"));
+                if (timeCostVal < 1 || timeCostVal > 100)
+                    return true;
+            } else
+                return true;
+        }
+        return false;
+    }
+
+    public void fixVariables() {
+        if (arguments.hasOption("memCost"))
+            Variables.MEMORYCOST = Integer.parseInt(arguments.getOptionValue("memCost")) * 1024;
+        if (arguments.hasOption("parallelism"))
+            Variables.MEMORYCOST = Integer.parseInt(arguments.getOptionValue("parallelism"));
+        if (arguments.hasOption("timeCost"))
+            Variables.MEMORYCOST = Integer.parseInt(arguments.getOptionValue("timeCost"));
+    }
+
     private boolean parseArgs(String[] args) {
         CommandLineParser parser = new DefaultParser();
         try {
@@ -43,19 +121,19 @@ public class Arguments {
     }
 
     private void buildOptions() {
-        Option decrypt = Option.builder("d").longOpt("decrypt").argName("file").hasArg().desc("file(s) to be decrypted")
+        Option decrypt = Option.builder("d").longOpt("decrypt").argName("file").hasArg().desc("file to be decrypted")
                 .build();
         options.addOption(decrypt);
-        Option encrypt = Option.builder("e").longOpt("encrypt").argName("file").hasArg().desc("file(s) to be encrypted")
+        Option encrypt = Option.builder("e").longOpt("encrypt").argName("file").hasArg().desc("file to be encrypted")
                 .build();
         options.addOption(encrypt);
-        Option memCost = Option.builder("m").argName("SIZE").hasArg()
-                .desc("password hashing memory cost in KB (default = 65536)").build();
+        Option memCost = Option.builder("m").longOpt("memCost").argName("SIZE").hasArg()
+                .desc("password hashing memory cost in MB (default = 64)").build();
         options.addOption(memCost);
-        Option parallelism = Option.builder("p").argName("NUMBER").hasArg()
+        Option parallelism = Option.builder("p").longOpt("parallelism").argName("NUMBER").hasArg()
                 .desc("number of lanes and threads to be used for password hashing (default = 4)").build();
         options.addOption(parallelism);
-        Option timeCost = Option.builder("t").argName("NUMBER").hasArg()
+        Option timeCost = Option.builder("t").longOpt("timeCost").argName("NUMBER").hasArg()
                 .desc("number of passes through memory (default = 10)").build();
         options.addOption(timeCost);
         Option help = new Option("h", "help", false, "prints this message");
--- src/main/java/jcloudcrypt/JCloudCrypt.java
+++ src/main/java/jcloudcrypt/JCloudCrypt.java
@@ -1,5 +1,9 @@
 package jcloudcrypt;
 
+import java.io.Console;
+import java.io.File;
+import java.util.Arrays;
+
 //Filename limit is 260 chars in windows, 255 in most linux filesystems
 //Java 2 bytes per char
 
@@ -32,16 +36,47 @@ public class JCloudCrypt {
     }
 
     public static int processCall(Arguments arguments) {
-        int returnVal = 0;
-        if (checkForConflicts(arguments)) {
+        if (arguments.checkForConflicts()) {
             return 2;
         }
-        return returnVal;
+        if (arguments.checkOutOfMemBounds()) {
+            return 3;
+        }
+        if (arguments.checkOutOfParallelismBounds()) {
+            return 4;
+        }
+        if (arguments.checkOutOfTimeCostBounds()) {
+            return 5;
+        }
+        arguments.fixVariables();
+        Console console = System.console();
+        boolean matches = false;
+        char[] password = null;
+        char[] passwordVerify = null;
+        while (!matches) {
+            password = console.readPassword("Password: "); // Gets cleared in Encryption / Decryption method
+            passwordVerify = console.readPassword("Verify Password: ");
+            matches = Arrays.equals(password, passwordVerify);
+        }
+        if (passwordVerify != null)
+            Arrays.fill(passwordVerify, ' ');
+        String filePath = arguments.getFilePath();
+        if (checkFileExists(filePath)) {
+            int returnVal = 0;
+            if (arguments.getSelection() == 'e') {
+                Encrypt encryption = new Encrypt();
+                returnVal = encryption.encryptFile(password, filePath, false);
+            } else if (arguments.getSelection() == 'd') {
+                Decrypt decryption = new Decrypt();
+                returnVal = decryption.decryptFile(password, filePath);
+            }
+            return returnVal;
+        } else
+            return 6;
     }
 
-    private static boolean checkForConflicts(Arguments arguments) {
-        if (arguments.hasOption("encrypt") && arguments.hasOption("decrypt"))
-            return true;
-        return false;
+    private static boolean checkFileExists(String filePath) {
+        File file = new File(filePath);
+        return file.isFile();
     }
 }
